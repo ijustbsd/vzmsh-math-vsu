efaab7d9236521a4ec95060219f422345faf04aa
Sergey Baboshin <ijustbsd@gmail.com>
2020-09-21 15:13:33 +0300

    Add participants list downloading on build

--- exec.sh
+++ exec.sh
@@ -1,3 +1,8 @@
 #!bin/sh
+
+wget -O "data.csv" "https://docs.google.com/spreadsheets/d/${GDRIVE_DOCUMENT_ID}/export?format=csv&gid=${GDRIVE_SHEET_ID}"
+
+python3 formatter.py
+
 cd /pelican
 pelican -d content
---
+++ formatter.py
@@ -0,0 +1,43 @@
+import csv
+
+INPUT_CSV = 'data.csv'
+OUTPUT_CSV = 'pelican/participants.csv'
+
+
+def str_formatter(x):
+    extra = {
+        'нет': '-',
+        '': '-'
+    }
+    # делаем замены некоторых строк фиксированными значениями
+    if x in extra:
+        return extra[x]
+    # меняем двойные пробелы на одинарные
+    x = x.replace('  ', ' ')
+    # если строка записана на 2-х языках через "/", то удаляем 2-ой язык
+    x = x.split('/')[0]
+    # если строка заканчивается пробелом, то удаляем его
+    x = x[:-1] if x[-1] == ' ' else x
+    return x
+
+
+result = []
+with open(INPUT_CSV) as csvfile:
+    reader = csv.reader(csvfile)
+    # скипаем заголовки столбцов
+    next(reader)
+    for row in reader:
+        # фамилия, имя, отчество, город, тема доклада
+        result.append([row[1], row[2], row[3], row[4], row[11]])
+# форматируем каждый столбец в строке
+result = [tuple(map(str_formatter, x)) for x in result]
+# убираем дубликаты
+result = set(result)
+# сортируем по алфавиту, учитывая ФИО и тему
+result = sorted(result, key=lambda x: ''.join(x))
+
+# сохраняем обработанные данные
+with open(OUTPUT_CSV, 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    for x in result:
+        writer.writerow(x)
